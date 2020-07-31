abb4873593bf0b43ce6409c400ab8ffba26fe3c9
Valentina E <tiyahelfrih@gmail.com>
2020-07-31 19:47:41 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,6 @@
 require 'pry'
 
 def hello(array)
-  i = 0
-  while i < array.length
-  binding.pry
-    yield(array[i])
-    i += 1
-  end
-end
+  
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
